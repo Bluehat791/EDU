07f87f73d420b072a5626fb10f971051bce9c619
Anatoliy Shchepetkov <tolick.shepetkov61@gmail.com>
2024-12-08 14:39:41 +0300

    SplayTree full implementation

--- FPMI/AVLTree.cpp
+++ FPMI/AVLTree.cpp
@@ -1,261 +1,72 @@
-#include<algorithm>
-#include<iostream>
-
+#include <bits/stdc++.h>
 using namespace std;
 
-template <typename T>
-class AVLNode
-{
+class Solution {
 public:
-    T key;
-    AVLNode* left;
-    AVLNode* right;
-    int height;
-
-    AVLNode(T k)
-        : key(k), left(nullptr),right(nullptr), height(1)
-        {}    
-};
-
-template <typename T>
-class AVLTree
-{
-private:
-
-    AVLNode<T>* root;
-
-    int height(AVLNode<T>* node)
-    {
-        if(node == nullptr)
-            return 0;
-        return node->height;
+    vector<int>tree;
+    void build(int node,int start,int end) {
+        if(start==end) {
+            tree[node]=1; 
+            return;
+        }
+        int mid=(start+end)/2;
+        build(2*node,start,mid);
+        build(2*node+1,mid+1,end);
+        tree[node]=tree[2*node]+tree[2*node+1];
     }
 
-    int balanceFactor(AVLNode<T>* node)
-    {
-        if(node == nullptr)
-            return 0;
-        return height(node->left) - height(node->right);
-    }
-
-    AVLNode<T>* rightRotate(AVLNode<T>* y)
-    {
-        AVLNode<T>* x = y->left;
-        AVLNode<T>* T2 = x->right;
-
-        x->right = y;
-        y->left = T2;
-
-        y->height = max(height(y->left),height(y->right))+1;
-        x->height = max(height(x->left),height(x->right))+1;
-
-        return x;
-    }
-
-    AVLNode<T>* leftRotate(AVLNode<T>* x)
-    {
-        AVLNode<T>* y = x->right;
-        AVLNode<T>* T2 = y->left;
-
-        y->left = x;
-        x->right = T2;
-
-        y->height = max(height(y->left),height(y->right))+1;
-        x->height = max(height(x->left),height(x->right))+1;
-
-        return y;
-    }
-
-    AVLNode<T>* insert(AVLNode<T>* node, T key)
-    {
-        if(node == nullptr)
-            return new AVLNode<T>(key);
-
-        if(key<node->key)
-            node->left = insert(node->left,key);
-        else if (key<node->key)
-            node->right = insert(node->right,key);
-        else
-            return node;
-
-        node->height = 1 + max(height(node->left), height(node->right));
-
-        int balance = balanceFactor(node);
-
-        if (balance > 1 && key < node->left->key)
-        {
-            return rightRotate(node);
+    int query(int node,int start,int end, int k) {
+        if(start==end) {
+            return start;
         }
-        if (balance < -1 && key > node->right->key)
-        {
-            return leftRotate(node);
-        }
-
-        if (balance > 1 && key > node->left->key)
-        {
-            node->left = leftRotate(node->left);
-            return rightRotate(node);
-        }
-
-        if (balance < -1 && key < node->right->key)
-        {
-            node->right = rightRotate(node->right);
-            return leftRotate(node);
-        }
-        
-        return node;    
-    }
-
-    AVLNode<T>* minValueNode(AVLNode<T>* node)
-    {
-        AVLNode<T>* current = node;
-        while(current->left != nullptr)
-            current = current->left;
-        return current;
-    }
-
-    AVLNode<T>* deleteNode(AVLNode<T>* root, T key)
-    {
-        if (root == nullptr)
-        {
-            return root;
-        }
-
-        if (key < root->key)
-        {
-            root->left= deleteNode(root->left,key);
-        } else if (key > root->key)
-            root->right = deleteNode(root->right,key);
-        else {
-            if ((root->left == nullptr)||(root->right == nullptr))
-            {
-                AVLNode<T>* temp = root->left ? root->left : root->right;
-
-                if (temp == nullptr)
-                {
-                    temp = root;
-                    root = nullptr;
-                }
-                else
-                    *root = *temp;
-                delete temp;
-            }
-            else {
-
-                AVLNode<T>* temp = minValueNode(root->right);
-                root->key = temp->key;
-                root->right = deleteNode(root->right,temp->key);
-            }
-            
-        }
-        
-        
-        if(root == nullptr)
-            return root;
-
-        root->height = 1 + max(height(root->left), height(root->right));
-
-        int balance = balanceFactor(root);
-
-
-        if (balance > 1 && balanceFactor(root->left) >= 0)
-        {
-            return rightRotate(root);
-        }
-
-        if (balance > 1 && balanceFactor(root->left)<0)
-        {
-            root->left = leftRotate(root);
-            return rightRotate(root);
-        }
-
-        if (balance < -1 && balanceFactor(root->right) <= 0)
-        {
-            return leftRotate(root);
-        }
-
-        if (balance < -1 && balanceFactor(root->right) > 0)
-        {
-            root->right = rightRotate(root->right);
-            return leftRotate(root);
-        }
-        
-        return root;
-    }
-
-    void inorder(AVLNode<T>* root)
-    {
-        if (root!=nullptr)
-        {
-            inorder(root->left);
-            cout << root->key << " ";
-            inorder(root->right);
+        int mid=(start+end)/2;
+        int left=tree[2*node];
+        int right=tree[2*node+1];
+        if(k>left){
+            return query(2*node+1,mid+1,end, k-left);
+        }else{
+            return query(2*node,start,mid,k);
         }
     }
 
-    bool search(AVLNode<T>* root, T key)
-    {
-        if (root==nullptr)
-        {
-            return false;
+    void update(int node,int start,int end,int idx) {
+        if(start==end) {
+            tree[node]=0; 
+            return;
         }
-        if (root->key == key)
-        {
-            return true;
+        int mid=(start+end)/2;
+        if(idx<=mid){
+            update(2*node,start,mid,idx);
+        }else{
+            update(2*node+1,mid+1,end,idx);
         }
-        if (key<root->key)
-        {
-            return search(root->left, key);
+        tree[node]=tree[2*node]+tree[2*node+1];
+    }
+    vector<vector<int>> reconstructQueue(vector<vector<int>>& people) {
+        sort(people.begin(), people.end(),[](vector<int>a,vector<int>b){
+            if(a[0]!=b[0]) return a[0]<b[0];
+            return a[1]>b[1];
+        });
+        int n=people.size();
+        tree.clear();
+        tree.resize(4*n+10);
+        vector<vector<int>> res(people.size(), vector<int>());
+        build(1,0,n-1);
+        for(int i = 0; i < people.size(); i++){
+            int j=query(1,0,n-1,people[i][1]+1);
+            res[j]=people[i];
+            update(1,0,n-1,j);
         }
-        return search(root->right, key);
-        
-    }
-
-public:
-
-    AVLTree() : root(nullptr)
-    {}
-
-    void insert(T key) {
-        root = insert(root,key);
-    }
-
-    void remove(T key) {
-        root = deleteNode(root,key);
-    }
-
-    bool search(T key){
-        return search(root,key);
-    }
-
-    void printInorder() {
-        inorder(root);
-        cout << endl;
+        return res;
     }
 };
 
+int main() {
+    Solution sol;
 
+    vector<vector<int>> tmp = {{7,0},{4,4},{7,1},{5,0},{6,1},{5,2}};
 
-int main()
-{
-    AVLTree<int> avl;
+    vector<vector<int>> res = sol.reconstructQueue(tmp);
 
-    avl.insert(10);
-    avl.insert(20);
-    avl.insert(30);
-    avl.insert(40);
-    avl.insert(50);
-    avl.insert(25);
-
-    cout << "Inorder traversal of the AVL tree: ";
-    avl.printInorder();
-
-    avl.remove(30);
-    cout << "Inorder traversal after removing 30: ";
-    avl.printInorder();
-
-    cout << "Is 25 on the tree? " << (avl.search(25)? "Yes" : "No") << endl;
-    cout << "Is 30 on the tree? " << (avl.search(30)? "Yes" : "No") << endl;
-
-    return 0;
+    cout << "fsffsf\n";
 }
